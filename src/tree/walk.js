0e59b4258f3dc0c660214809f7b54a7207e1de01
Kindy Lin <kindy.lin@gmail.com>
2017-05-02 15:56:29 +0800

    update walkTree

--- src/tree/walk.js
+++ src/tree/walk.js
@@ -2,6 +2,13 @@
  * childrenFn > childrenKey
  *
  * fn: (node, ctx, pNode) => void;
+ *   ctx: {
+ *     index: 1,
+ *     level: 0,
+ *   }
+ *
+ * walkTree([], (node, ctx, pNode) => {
+ * });
  */
 export default function walkTree(
   root,
@@ -13,39 +20,45 @@ export default function walkTree(
     rootAsChildren = false,
     skipRoot = false,
     ctx = {},
+    level = 0,
   } = {}
 ) {
   if (childrenFn === null) {
     childrenFn = node => node[childrenKey];
   }
 
-  let processNode = (node, pNode) => {
+  let processNode = (node, pNode, level, index) => {
     if (!childFirst) {
+      ctx.level = level;
+      ctx.index = index;
       fn(node, ctx, pNode);
     }
 
-    processChildren(node);
+    processChildren(node, level);
 
     if (childFirst) {
+      ctx.level = level;
+      ctx.index = index;
       fn(node, ctx, pNode);
     }
   };
-  let processChildren = pNode => {
+  let processChildren = (pNode, level) => {
     let children = childrenFn(pNode);
     if (children && children.length) {
-      children.forEach(node => processNode(node, pNode));
+      children.forEach((node, index) => processNode(node, pNode, level + 1, index));
     }
   };
 
   if (rootAsChildren) {
     if (root && root.length) {
-      root.forEach(node => processNode(node, null));
+      root.forEach((node, index) => processNode(node, null, level, index));
     }
   } else {
     if (skipRoot) {
-      processChildren(root);
+      processChildren(root, level);
     } else {
-      processNode(root, null);
+      processNode(root, null, level, -1);
     }
   }
 }
+
