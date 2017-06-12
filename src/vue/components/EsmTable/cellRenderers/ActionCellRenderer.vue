c51cf75f6b1ea1d6aa0d62d6adc987db33f11a93
Kindy Lin <kindy.lin@gmail.com>
2017-06-12 23:29:25 +0800

    make actioncell support visible check

--- src/vue/components/EsmTable/cellRenderers/ActionCellRenderer.vue
+++ src/vue/components/EsmTable/cellRenderers/ActionCellRenderer.vue
@@ -4,18 +4,34 @@
       v-for="action in params.actions",
       @click.prevent="doClick(action)",
       v-text="action.label",
+      v-if="isVisible(action)",
       :class="{[`btn-${action.style || 'default'}`]: true, [`btn-${action.size || 'xs'}`]: true}"
     )
 </template>
 
 
 <script>
+  import {isFunction} from 'lodash';
+
   export default {
     props: {params: {}},
 
     methods: {
       doClick(action) {
         action.doAction && action.doAction({action, params: this.params});
+      },
+
+      isVisible(action) {
+        const {visible = null} = action;
+        if (visible === null) {
+          return true;
+        }
+
+        if (isFunction(visible)) {
+          return visible({action, params: this.params});
+        }
+
+        return visible;
       },
     },
   };
